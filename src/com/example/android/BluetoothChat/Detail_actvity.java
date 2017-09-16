a8a81148470b82cfffaeffb5b9bde3a100f5015b
arash kayhani <arkayhani@gmail.com>
2017-09-16 01:08:30 -0300

    upload

---
+++ src/com/example/android/BluetoothChat/Detail_actvity.java
@@ -0,0 +1,16 @@
+package com.example.android.BluetoothChat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Detail_actvity extends Activity {
+	
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	setContentView(R.layout.detail);
+	
+	super.onCreate(savedInstanceState);
+	
+}
+}
